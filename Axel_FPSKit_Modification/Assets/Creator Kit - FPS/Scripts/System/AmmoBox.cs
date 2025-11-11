724e0596cdc4672e0dbcfcc29b080d1fa58da9c1
grace-siegwald <siegwald@colum.edu>
2025-11-10 22:14:20 -0600

    Attempted implementation of ground collision for AmmoBox

--- Axel_FPSKit_Modification/Assets/Creator Kit - FPS/Scripts/System/AmmoBox.cs
+++ Axel_FPSKit_Modification/Assets/Creator Kit - FPS/Scripts/System/AmmoBox.cs
@@ -4,6 +4,7 @@ using System.Numerics;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
+using Quaternion = UnityEngine.Quaternion;
 
 [RequireComponent(typeof(Collider))] 
 [RequireComponent(typeof(Rigidbody))]
@@ -15,21 +16,41 @@ public class AmmoBox : MonoBehaviour
     public int amount;
 
     [Header("Physics Settings")]
-    public float dropForce = 2.0f;
     private Rigidbody rb;
     private bool hasLanded;
+    private GameObject collisionTrigger;
 
     void Start()
     {
-        rb = GetComponent<RigidBody>();
-        rb.AddForce(Vector3.up * dropForce, ForceMode.Impulse);
-    }
-    void Reset()
-    {
-        gameObject.layer = LayerMask.NameToLayer("PlayerCollisionOnly");
-        GetComponent<Collider>().isTrigger = true;
+        rb = GetComponent<Rigidbody>();
+
+        // Creates a child object to handle the trigger collider
+        collisionTrigger = new GameObject("TriggerCollider");
+        collisionTrigger.transform.SetParent(transform);
+        collisionTrigger.transform.localPosition = Vector3.zero;
+        collisionTrigger.transform.localRotation = Quaternion.identity;
+        collisionTrigger.layer = LayerMask.NameToLayer("PlayerCollisionOnly");
+        BoxCollider triggerCollider = collisionTrigger.AddComponent<BoxCollider>();
+        triggerCollider.isTrigger = true;
+        triggerCollider.size = new Vector3(2.0f, 2.0f, 2.0f);
+
     }
 
+    void Reset()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Default");
+    }
+
+    // Detects when the ammo hit's the ground!
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!hasLanded)
+        {
+            hasLanded = true;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+    
     void OnTriggerEnter(Collider other)
     {
         Controller c = other.GetComponent<Controller>();
